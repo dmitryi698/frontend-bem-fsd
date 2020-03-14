95d3db8ae602872c086c5832e0f995577dec1dfb
Dmitry <dima@mail.com>
2020-03-14 20:09:32 +0400

    Update block menu js

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,1 +1,18 @@
 import './menu.scss';
+
+$(() => {
+  $('.js-menu__link').click(() => {
+
+    $('.menu__submenu').toggleClass('menu__submenu_activated');
+    
+  });
+
+  $(document).mouseup((e) => {
+    
+    if (!$('.menu__submenu').is(e.target) && $('.menu__submenu').has(e.target).length === 0) {
+
+      $('.menu__submenu').removeClass('menu__submenu_activated');
+
+    }
+  });
+});
