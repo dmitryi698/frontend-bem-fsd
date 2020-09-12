eb8e73bd7bed5befa16341a6a3ea304c9acc39ff
Dmitry <dima@mail.com>
2020-09-12 09:04:24 +0400

    Update date-dropdown: js, css, pug

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -1,79 +1,78 @@
 import './date-dropdown.scss';
 
-$(function ($) {
-  $('.js-date-dropdown__input_start').inputmask({
-    alias: 'datetime',
-    inputFormat: 'dd.mm.yyyy',
-    placeholder: 'ДД.ММ.ГГГГ'
-  });
-
-  $('.js-date-dropdown__input_end').inputmask({
-    alias: 'datetime',
-    inputFormat: 'dd.mm.yyyy',
-    placeholder: 'ДД.ММ.ГГГГ'
-  });
-
-});
 
 
+class Datepicker {
+  constructor($component) {
+    this.$component = $component;
 
-// Добавил в настройку позиционированию posOffset,
-// регулирует положение календаря относительно инпута по горизонтали
-const prewCustom = 'arrow_back';
-const nextCustom = 'arrow_forward';
-
-let $start = $('.js-date-dropdown__input_start'),
-    $end = $('.js-date-dropdown__input_end'),
-    $filterDate = $('.js-date-dropdown__input');
-
-$start.datepicker({
-  clearButton: true,
-  posOffset: 16,
-  multipleDatesSeparator: ' - ',
-  dateFormat: 'dd.mm.yyyy',
-  prevHtml: prewCustom,
-  nextHtml: nextCustom,
-  navTitles: {
-    days: 'MM yyyy'
-  },  onSelect: function (fd, date) {
-    $end.data('datepicker')
-      .update('minDate', date);
-
-    $end.focus();
+    this.render();
   }
-})
 
-$end.datepicker({
-  position: 'bottom right',
-  posOffset: 48,
-  clearButton: true,
-  multipleDatesSeparator: ' - ',
-  dateFormat: 'dd.mm.yyyy',
-  prevHtml: prewCustom,
-  nextHtml: nextCustom,
-  navTitles: {
-    days: 'MM yyyy'
-  },
-  onSelect: function (fd, date) {
-    $start.data('datepicker')
-      .update('maxDate', date)
+  render() {
+
+    let $start = $('.js-date-dropdown__input_start', this.$component);
+    let $end = $('.js-date-dropdown__input_end', this.$component);
+    let $filterDate = $('.js-date-dropdown__input', this.$component);
+    let prewCustom = 'arrow_back';
+    let nextCustom = 'arrow_forward';
+    let okButton = '<span class="datepicker--button" data-action="hide">Применить</span>';
+
+    $start.datepicker({
+      offset: 5,
+      posOffset: 1,
+      dateFormat: 'dd.mm.yyyy',
+      clearButton: true,
+      prevHtml: prewCustom,
+      nextHtml: nextCustom,
+      navTitles: {
+        days: 'MM yyyy'
+      }, onSelect: function (fd, date) {
+        $end.data('datepicker')
+          .update('minDate', date);
+        $end.focus();
+      }
+    });
+
+    $end.datepicker({
+      position: 'bottom right',
+      offset: 5,
+      posOffset: 35,
+      dateFormat: 'dd.mm.yyyy',
+      clearButton: true,
+      prevHtml: prewCustom,
+      nextHtml: nextCustom,
+      navTitles: {
+        days: 'MM yyyy'
+      },
+      onSelect: function (fd, date) {
+        $start.data('datepicker')
+          .update('maxDate', date)
+      }
+    });
+
+    $('.datepicker--button[data-action="clear"]').each(function (index) { $(okButton).insertAfter($(this)); });
+
+    $filterDate.datepicker({
+      offset: 5,
+      posOffset: 0,
+      range: true,
+      clearButton: true,
+      multipleDatesSeparator: ' - ',
+      dateFormat: 'dd M',
+      prevHtml: prewCustom,
+      nextHtml: nextCustom,
+      navTitles: {
+        days: 'MM yyyy'
+      }
+    });
+
+
   }
-})
-
-$filterDate.datepicker({
-  posOffset: 16,
-  range: true,
-  clearButton: true,
-  multipleDatesSeparator: ' - ',
-  dateFormat: 'dd M',
-  prevHtml: prewCustom,
-  nextHtml: nextCustom,
-  navTitles: {
-    days: 'MM yyyy'
-  },
-})
+}
 
 
-// Add confirm button
-var okButton = '<span class="datepicker--button" data-action="hide">Применить</span>';
-$('.datepicker--button[data-action="clear"]').each(function (index) { $(okButton).insertAfter($(this)); });
+  $(() => {
+    let dateFilter = new Datepicker('.js-date-dropdown_theme_filter');
+    let dateDropdown = new Datepicker('.js-date-dropdown');
+  });
