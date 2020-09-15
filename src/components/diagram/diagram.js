c90f93fdba07e0a3023a95edb9836cb2058baf25
Dmitry <dima@mail.com>
2020-09-16 00:30:57 +0400

    created block diagram: pug, scss, html

--- src/components/diagram/diagram.js
+++ src/components/diagram/diagram.js
@@ -1,1 +1,29 @@
 import './diagram.scss';
+
+class Diagram {
+  constructor($component) {
+    this.$component = $component;
+
+    this.render();
+  }
+
+  render() {
+
+    
+    let circles = document.querySelectorAll('.diagram__circle');
+    let listItem = document.querySelectorAll('.js-diagram__item');
+
+    listItem.forEach(function (item, index) {
+      item.addEventListener('mouseover', function () {
+        circles[index].classList.add('diagram__circle_hovered');
+      });
+
+      item.addEventListener('mouseout', function () {
+        circles[index].classList.remove('diagram__circle_hovered');
+      });
+    });
+ 
+  }
+}
+
+let diagram = new Diagram('.js-diagram')
